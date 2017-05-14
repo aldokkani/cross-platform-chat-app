ca304bd7e8eed65f9daa7fe8d5721f77f31df02f
mohamed <muhammed_magdy838@yahoo.com>
2017-05-14 21:55:28 +0200

    private chat

--- ChatApp/www/js/controllers/activeusers.js
+++ ChatApp/www/js/controllers/activeusers.js
@@ -1,4 +1,4 @@
-angular.module('ChatApp').controller('activeusers', function($scope, $rootScope, $timeout, $state){
+angular.module('ChatApp').controller('activeusers', function($location, $scope, $rootScope, $timeout, $state){
     console.log("ActiveUsersCtrl");
     $scope.onlineUsers = [];
     var online = true;
@@ -18,6 +18,12 @@ angular.module('ChatApp').controller('activeusers', function($scope, $rootScope,
             socket.emit('login', $rootScope.user.username);
             online = true;
         }
+    }
+
+    $scope.privateMsg = function(user){
+        $rootScope.privateUser = user;
+        $state.go('app.privatechat')
+           
     }
 
 });
